bc3b814af94b093869763f4a9a6f9af5eefd8596
BGStone <2488708491@qq.com>
2020-02-07 14:07:22 +0800

    1.添加日志；2.刷新日志，将最新的token替换掉

--- image-recognition/src/main/java/com/doctor/assistant/ImageRecognition/service/ImageRecognitionService.java
+++ image-recognition/src/main/java/com/doctor/assistant/ImageRecognition/service/ImageRecognitionService.java
@@ -15,26 +15,35 @@ import com.doctor.assistant.ImageRecognition.utils.Results;
 import com.doctor.assistant.ImageRecognition.utils.UUIDUtil;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.client.methods.HttpPost;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.time.LocalTime;
 import java.util.*;
 import java.util.function.BinaryOperator;
 
 @RefreshScope
 @Service
 public class ImageRecognitionService {
+    final static Logger log = LoggerFactory.getLogger(ImageRecognitionService.class);
+
+    @Value("${zzs_BaiduApiURL}")
+    private String zzsBaiduApiURL;
 
     @Value("${zzs_APIKey}")
     private String zzsAPIKey;
     @Value("${zzs_SecretKey}")
     private String zzsSecretKey;
-    @Value(value="${access_token}") // "access_token":"24.d967bbf7c5ad6c77cfdd614d3ef352a2.2592000.1578992008.282335-17963898"
-    private String accessToken;    // 返回信息：{"refresh_token":"25.257f0dbe0061e321c130e61992dd1159.315360000.1891760008.282335-17963898","expires_in":2592000,"session_key":"9mzdX+OCakh4wx60ybtuGXoSLksEhS7vnd\/DwS2GSmSRbwssN9tSWfueQw81cQVvL9YZ5tL5BPCbw8MnMu\/nN5GJ2WO\/cg==","access_token":"24.d967bbf7c5ad6c77cfdd614d3ef352a2.2592000.1578992008.282335-17963898","scope":"public vis-ocr_ocr brain_ocr_scope brain_ocr_general brain_ocr_general_basic vis-ocr_business_license brain_ocr_webimage brain_all_scope brain_ocr_idcard brain_ocr_driving_license brain_ocr_vehicle_license vis-ocr_plate_number brain_solution brain_ocr_plate_number brain_ocr_accurate brain_ocr_accurate_basic brain_ocr_receipt brain_ocr_business_license brain_solution_iocr brain_qrcode brain_ocr_handwriting brain_ocr_passport brain_ocr_vat_invoice brain_numbers brain_ocr_business_card brain_ocr_train_ticket brain_ocr_taxi_receipt vis-ocr_household_register vis-ocr_vis-classify_birth_certificate vis-ocr_\u53f0\u6e7e\u901a\u884c\u8bc1 vis-ocr_\u6e2f\u6fb3\u901a\u884c\u8bc1 vis-ocr_\u673a\u52a8\u8f66\u68c0\u9a8c\u5408\u683c\u8bc1\u8bc6\u522b vis-ocr_\u8f66\u8f86vin\u7801\u8bc6\u522b vis-ocr_\u5b9a\u989d\u53d1\u7968\u8bc6\u522b vis-ocr_\u4fdd\u5355\u8bc6\u522b brain_ocr_vin brain_ocr_quota_invoice brain_ocr_birth_certificate brain_ocr_household_register brain_ocr_HK_Macau_pass brain_ocr_taiwan_pass brain_ocr_vehicle_certificate brain_ocr_insurance_doc wise_adapt lebo_resource_base lightservice_public hetu_basic lightcms_map_poi kaidian_kaidian ApsMisTest_Test\u6743\u9650 vis-classify_flower lpq_\u5f00\u653e cop_helloScope ApsMis_fangdi_permission smartapp_snsapi_base iop_autocar oauth_tp_app smartapp_smart_game_openapi oauth_sessionkey smartapp_swanid_verify smartapp_opensource_openapi smartapp_opensource_recapi fake_face_detect_\u5f00\u653eScope vis-ocr_\u865a\u62df\u4eba\u7269\u52a9\u7406 idl-video_\u865a\u62df\u4eba\u7269\u52a9\u7406","session_secret":"762ae94eb8b8fdc5de6e516415d64e03"}
-    @Value(value="${tokenUrl}") // "access_token":"24.d967bbf7c5ad6c77cfdd614d3ef352a2.2592000.1578992008.282335-17963898"
+    @Value(value="${access_token}")
+    private String accessToken;
+    @Value(value="${tokenUrl}")
     private String tokenUrl;
     @Value(value="${refresh_url}")
     private String refreshUrl;
@@ -54,6 +63,62 @@ public class ImageRecognitionService {
 
     public String getAccessToken(){
         return accessToken;
+    }
+    public String setAccessToken(String access_token){
+        return this.accessToken = access_token;
+    }
+
+    public InvoiceMain recogeImage(MultipartFile[] images){
+
+        long timeBefore = LocalTime.now().toNanoOfDay();
+        long timeSecondBefore = LocalTime.now().toSecondOfDay();
+        List<String> list = new ArrayList<String>();
+        String encodeFileStr = null;
+        StringBuilder stringBuilder = null;
+        InvoiceMain invoiceMain = null;
+
+        if (images != null && images.length > 0 && !images[0].isEmpty()) {
+            for (int i = 0; i < images.length; i++) {
+                stringBuilder = new StringBuilder();
+                MultipartFile file = images[i];
+                // 保存文件
+//                list = saveFile(request, file, list);
+                final Base64.Encoder encoder = Base64.getEncoder();
+                try {
+                    encodeFileStr = encoder.encodeToString(file.getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Map<String,String> headerMap = new HashMap<>();
+                headerMap.put("Content-Type", "application/x-www-form-urlencoded");
+                // image	是	string	-	图像数据，
+                // base64编码后进行urlencode，
+                // 要求base64编码和urlencode后大小不超过4M，最短边至少15px，最长边最大4096px,
+                // 支持jpg/jpeg/png/bmp格式
+                Map<String,String> paramMap = new HashMap<>();
+                paramMap.put("image", encodeFileStr);
+                // accuracy	否	string	normal/high
+                // normal（默认配置）对应普通精度模型，识别速度较快，在四要素的准确率上和 high 模型保持一致，
+                // high对应高精度识别模型，相应的时延会增加，因为超时导致失败的情况也会增加（错误码282000）
+                paramMap.put("accuracy","normal");
+                // type	否	string	normal/roll	进行识别的增值税发票类型，默认为 normal，可缺省
+                //- normal：可识别增值税普票、专票、电子发票
+                //- roll：可识别增值税卷票
+                paramMap.put("type","normal");
+                paramMap.put("access_token",accessToken);
+//                System.out.println("zzsBaiduApiURL:" + zzsBaiduApiURL);
+                stringBuilder.append("\r\n");
+//                stringBuilder.append();
+                invoiceMain = this.callBaiduImageRecognition(zzsBaiduApiURL, headerMap, paramMap);
+                long timeMid = LocalTime.now().toNanoOfDay();
+                long timeSecondMid = LocalTime.now().toSecondOfDay();
+                System.out.println("至第"+(i+1)+"张:");
+                System.out.println("用时秒数:"+(timeSecondMid - timeSecondBefore));
+                System.out.println("用时毫秒：" +(timeMid - timeBefore));
+            }
+        }
+
+        return invoiceMain;
     }
 
     public InvoiceMain callBaiduImageRecognition(String url, Map<String, String> headerMap, Map<String, String> paramMap){
