13124272b3293433f9820aed5e022e3aa50c0030
BGStone <2488708491@qq.com>
2020-08-07 21:03:43 +0800

    1.配置eureka-server; 2.test依赖； 3.添加新模块，完善相关依赖。

--- image-recognition/src/test/java/com/doctor/assistant/ImageRecognition/ImageRecognitionApplicationTests.java
+++ image-recognition/src/test/java/com/doctor/assistant/ImageRecognition/ImageRecognitionApplicationTests.java
@@ -1,7 +1,10 @@
 package com.doctor.assistant.ImageRecognition;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ImageRecognitionApplicationTests {
 
