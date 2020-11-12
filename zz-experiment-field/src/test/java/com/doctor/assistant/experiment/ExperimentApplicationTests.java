62356a35cc2e66d9c8548d22852d49588fa50684
BGStone <2488708491@qq.com>
2020-11-12 17:16:27 +0800

    1.excel 导入 测试数据；2.①字符替换；②单链表模拟手写加法的过程；③动态规划可以1跳、2跳、3跳，则n级有多少种方法？

--- zz-experiment-field/src/test/java/com/doctor/assistant/experiment/ExperimentApplicationTests.java
+++ zz-experiment-field/src/test/java/com/doctor/assistant/experiment/ExperimentApplicationTests.java
@@ -1,12 +1,45 @@
 package com.doctor.assistant.experiment;
 
+import com.doctor.assistant.experiment.czy.entity.EvalKpiSimValue;
+import com.doctor.assistant.experiment.czy.poi.excel.entity.params.ImportParams;
+import com.doctor.assistant.experiment.czy.poi.excel.utils.ExcelImportUtil;
+import com.doctor.assistant.experiment.czy.repository.EvalKpiSimValueRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
-class ExperimentApplicationTests {
+@RunWith(value = SpringRunner.class)
+public class ExperimentApplicationTests {
+    @Autowired
+    EvalKpiSimValueRepository repository;
+//    @Test
+    void contextLoads() {
+    }
 
     @Test
-    void contextLoads() {
+    public void testRepository() throws Exception {
+//        repository.saveAll(readFile());
+    }
+
+    private List<EvalKpiSimValue> readFile() throws Exception{
+        List<EvalKpiSimValue> list = new ArrayList<>();
+        String path = "C:\\Users\\Administrator\\Desktop\\message-kpi-value.xls";
+        File file = new File(path);
+        FileInputStream input = new FileInputStream(file);
+
+        ImportParams params = new ImportParams();
+        params.setTitleRows(2);
+        params.setHeadRows(2);
+        params.setNeedSave(true);
+        list =  ExcelImportUtil.importExcel(input, EvalKpiSimValue.class, params);
+        return list;
     }
 }
